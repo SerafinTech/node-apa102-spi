f8c00f318eda7c5c5c33e745bb1e65157cfa9c74
SerafinTech <jtserafin@gmail.com>
2017-03-11 18:09:16 -0500

    example.js
    example.js

--- example.js
+++ example.js
@@ -4,6 +4,9 @@ var Apa102spi = require('./apa102-spi')
 var LedDriver = new Apa102spi(3, 100)
 
 // setLedColor(n, brightness 0-31, red 0-255, green 0-255, blue 0-255)
+LedDriver.setLedColor(0, 31, 255, 0, 0)
+LedDriver.setLedColor(1, 31, 0, 255, 0)
+LedDriver.setLedColor(2, 31, 0, 0, 255)
 
 // send data to led string
 LedDriver.sendBuffer()
