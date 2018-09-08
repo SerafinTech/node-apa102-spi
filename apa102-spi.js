9fb6803e9522ef2fe1864f7b8a8a8c5013a3ab91
SerafinTech <jtserafin@gmail.com>
2018-09-08 00:56:09 -0400

    Added direct buffer writing

--- apa102-spi.js
+++ apa102-spi.js
@@ -8,8 +8,8 @@ function Apa102spi (stringLength, clockDivider) {
   clockDivider = typeof clockDivider !== 'undefined' ? clockDivider : 200
   this.bufferLength = stringLength * 4
   this.writeBuffer = Buffer.alloc(this.bufferLength, 'E0000000', 'hex')
-  this.bufferLength += 8
-  this.writeBuffer = Buffer.concat([Buffer.alloc(4, '00000000', 'hex'), this.writeBuffer, Buffer.alloc(4, 'FFFFFFFF', 'hex')], this.bufferLength)
+  this.bufferLength += 4
+  this.writeBuffer = Buffer.concat([Buffer.alloc(4, '00000000', 'hex'), this.writeBuffer], this.bufferLength)
   
   rpio.spiBegin()
   rpio.spiSetClockDivider(clockDivider)
@@ -26,6 +26,10 @@ Apa102spi.prototype.setLedColor = function (n, brightness, r, g, b) {
   this.writeBuffer[n + 1] = b
   this.writeBuffer[n + 2] = g
   this.writeBuffer[n + 3] = r
+}
+
+Apa102spi.prototype.setBuffer = function (ledBuffer) {
+  this.writeBuffer = Buffer.concat([Buffer.alloc(4, '00000000', 'hex'), ledBuffer], this.bufferLength)
 }
 
 module.exports = Apa102spi
